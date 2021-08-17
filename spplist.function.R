6eadaeba50c7eb22f3651defdc0fbedee8d4341f
Jon Bakker <jon-bakker@users.noreply.github.com>
2021-08-17 16:39:18 -0700

    spplist() function
    Simple function to tally the taxa within a site.  Data are summarized in a matrix:
    - Rows are taxa, sorted alphabetically by family and then by taxon.
    - Columns are years
    - Values are the number of entries for that taxon in that year.

---
+++ spplist.function.R
@@ -0,0 +1,11 @@
+# Function to tally species in a site (taxon x year matrix)
+spplist <- function(datafile = datafile, site_code = site_code) {
+  require(tidyverse)
+  temp <- datafile[datafile$site_code == site_code, ] %>%
+    group_by(Taxon, year) %>%
+    summarize(N = length(max_cover > 0), .groups = "keep") %>%
+    pivot_wider(names_from = year, values_from = N, names_sort = TRUE) %>%
+    merge(y = unique(datafile[ , c("Taxon", "Family", "live")]), by.y = "Taxon", all.y = FALSE)
+  temp[is.na(temp)] <- "."
+  with(temp, temp[ order(Family, Taxon), ])
+}
