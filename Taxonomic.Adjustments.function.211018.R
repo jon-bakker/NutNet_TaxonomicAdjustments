45e431857561ad5bfeaea39f325054033fdad0f1
Jon Bakker <jon-bakker@users.noreply.github.com>
2022-01-25 13:53:02 -0800

    Add files via upload

---
+++ Taxonomic.Adjustments.function.211018.R
@@ -0,0 +1,45 @@
+##### NUTNET TAXONOMIC ADJUSTMENTS
+## Based on analyses from 2017 NutNet workshop and previous efforts
+## JD Bakker et al
+## 220120
+
+## Separate taxonomic adjustments for each site to increase temporal consistency in naming
+
+## Based on data in 'full-cover-18-October-2021.csv'
+## Includes all sites with >= 4 years of data
+
+## Most changes are summarized in a CSV file (adjustments.needed)
+## Individual changes can be dropped by setting 'Omit' == "Yes" 
+
+Taxonomic.Adjustments <- function(datafile = datafile, adjustments.needed = adjustments.needed) {
+  
+  data1 <- datafile
+  
+  # Drop mosses, lichens, fungi
+  data1 <- data1[! data1$functional_group %in% c("BRYOPHYTE", "LICHEN", "CLUBMOSS", "LIVERWORT", "NON-LIVE") , ]
+  #some families not consistently identified to functional group
+  data1 <- data1[! data1$Family %in% c("Dicranaceae", "Lycopodiaceae", "Phallales", "Pottiaceae",
+                                       "Selaginellaceae", "Thuidiaceae", "MNIACEAE", "Polytrichaceae") , ]
+  
+  # Drop records not assigned to family
+  data1 <- data1[! data1$Family %in% c("NULL") , ]
+  
+  # Site-specific taxonomic adjustments
+  data2 <- merge(x = data1, by = c("site_code", "Taxon"), all.x = TRUE,
+                 y = adjustments.needed[ adjustments.needed$Omit == "No" , ], all.y = FALSE) %>%
+    mutate(NewTaxon = ifelse(is.na(NewTaxon), Taxon, NewTaxon),
+           Taxon = NULL, Notes = NULL, Omit = NULL) %>%
+    rename(Taxon = NewTaxon) %>%
+    filter(Taxon != "EXCLUDE")
+  
+  data2.check <- merge(x = data1, by = c("site_code", "Taxon"), all.x = FALSE,
+                 y = adjustments.needed[ adjustments.needed$Omit == "No" , ], all.y = TRUE) %>%
+    filter(is.na(max_cover))
+
+  temp <- data2 %>%
+    group_by(site_code, block, plot, subplot, trt, year, year_trt, Taxon, Family) %>%
+    summarize(max_cover = sum(max_cover), .groups = "keep")
+
+  temp
+  
+}
